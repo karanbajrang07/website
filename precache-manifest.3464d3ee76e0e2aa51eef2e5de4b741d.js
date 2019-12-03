1ef81536c7889b50d1bc0769f9e67214d332a3cf
Karan Bajrang Bishnoi <karanbk.bpgc@gmail.com>
2019-12-04 02:30:40 +0530

    Updates

---
+++ precache-manifest.3464d3ee76e0e2aa51eef2e5de4b741d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf6c20f91e377c3e57b5047724f03ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae567c376d2294339cc5",
+    "url": "/static/css/main.ac22edd8.chunk.css"
+  },
+  {
+    "revision": "ba1d435250077af02295",
+    "url": "/static/js/2.134e29c2.chunk.js"
+  },
+  {
+    "revision": "ae567c376d2294339cc5",
+    "url": "/static/js/main.17fcf968.chunk.js"
+  },
+  {
+    "revision": "427262b6771d3f49a7c5",
+    "url": "/static/js/runtime-main.4aea9da3.js"
+  },
+  {
+    "revision": "fe13f713c1a54fda302e5340b1e7cf2d",
+    "url": "/static/media/backg.fe13f713.jpg"
+  },
+  {
+    "revision": "f516bc82a1702fe17944ef7284dba7df",
+    "url": "/static/media/logo.f516bc82.JPG"
+  }
+]);
